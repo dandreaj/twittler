6b372408df9dc014fee2086bdba9f1d3df6df6ee
Jessica D'Andrea <jessica.dandrea@pwc.com>
2017-04-30 18:55:33 -0500

    Tweets update only newest tweets and added some style

--- data_manipulator.js
+++ data_manipulator.js
@@ -1,37 +1,55 @@
 $(document).ready(function() {
   var $body = $('body');
+  var tweetCounter = 0;
+  var endingIndex = 0;
 
   function updateTweets() {
     var index = streams.home.length - 1;
-    while (index >= 0) {
+    while (index >= endingIndex) {
       var tweet = streams.home[index];
-      var $tweet = $('<div><span class="createdAt"></span><button class="user"></button><span class="message"></span></div>');
-      $tweet.find('.createdAt').text(tweet.created_at);
+      var dateTime = tweet.created_at;
+      var timePassed = jQuery.timeago(dateTime);
+      var $tweet = $('<div><a class="user"></a><span class="createdAt"></span><p><span class="message"></span></p></span></div>');
+      $tweet.find('.createdAt').text(dateTime);
       $tweet.find('.user').val(tweet.user);
       $tweet.find('.user').text(tweet.user);
-      $tweet.find('.message').text(tweet.message);
-      // $tweet.text(tweet.created_at + '@' + tweet.user + ': ' + tweet.message);
-      $tweet.appendTo($body);
+      $tweet.find('.message').text('"'+tweet.message+'"');
+      $tweet.find('.timePassed').text(timePassed);
+      $tweet.appendTo("#tweetBody");
       index -= 1;
     }
+    endingIndex= streams.home.length;
   }
 
   function filterUsers(selectedUser) {
+    $('.user').each(function(){
+         if($(this).html()!==selectedUser)
+              $(this).parent().hide();
+         });
   }
 
   //Update tweets button
   $("#updateTweets").click(function() {
+    //need to show unhide tweets on update
     updateTweets();
+    $(document).ready(function() {
+      $('.user').click(function() {
+        var selectedUser = this.value;
+        filterUsers(selectedUser);
+
+      });
+    });
+
   });
 
   //Filter based on user selected
   $(document).ready(function() {
     $('.user').click(function() {
       var selectedUser = this.value;
-      $('.user').each(function(){
-           if($(this).html()!==selectedUser)
-                $(this).parent().hide();
-           });
+      filterUsers(selectedUser);
+
+      $("time.timeago").timeago();
+
     });
   });
 
